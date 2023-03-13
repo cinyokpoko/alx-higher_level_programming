c72399f952214f941361c05d17e479fbaa8a49a5
cinyokpoko <cinyokpoko177@gmai.com>
2023-03-13 12:55:34 +0000

    all tasks

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+(sentence):
+    if len(sentence) == 0:
+        list = [len(sentence), None]
+        new_tuplex = tuple(list)
+    else:
+        list = [len(sentence), sentence[0]]
+        new_tuplex = tuple(list)
+    return new_tuplex
+
